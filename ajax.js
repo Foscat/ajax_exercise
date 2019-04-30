dda667390c29e18c0c9eb3e948ee73447cc56efd
Kyle <foscat4o1k@outlook.com>
2019-04-29 21:44:14 -0500

    fetch working

--- ajax.js
+++ ajax.js
@@ -44,15 +44,16 @@ $("#get10post").on("click", function(){
 // 3) Get the comments from post with id of 12
 $("#get12com").on("click", function api(){
     $.ajax({
-        url:"http://jsonplaceholder.typicode.com/post/12/comments",
+        url:"http://jsonplaceholder.typicode.com/comments/?postId=12",
         method: "GET"
     }).then(function(res){
+        console.log(res)
         $("#apiDump").empty();
         for(var i=0; i < res.length; i++){
-            // console.log(res[i]);
+            console.log(res[i]);
             div = $("<div class='border border-dark m-1 p-1'>")
             h = $("<h3 class='border-bottom border-danger'>").text("ID: " + res[i].id + "\n Title: "+res[i].name);
-            var h2 = $("<h4>").text("Post Id: " + res[i].postId);
+            var h2 = $("<h4>").text("Post Id: " + res[i].userId + "Emailed to: " + res[i].email);
             p = $("<p>").text("Body: " + res[i].body);
 
             $(div).append(h, h2,p);
@@ -65,7 +66,7 @@ $("#get12com").on("click", function api(){
 // 4) Get all the posts from user with id of 2
 $("#get2").on("click", function api(){
     $.ajax({
-        url:"http://jsonplaceholder.typicode.com/user/2/posts",
+        url:"http://jsonplaceholder.typicode.com/posts?userId=2",
         method: "GET"
     }).then(function(res){
         $("#apiDump").empty();
@@ -128,7 +129,8 @@ $("#replacePost").on("click", function(){
             $("#apiDump").empty();
             console.log(res.responseJSON);
             div = $("<div class='border border-dark m-1 p-1'>")
-            h = $("<h3 class='border-bottom border-danger'>").text("ID: " + res.responseJSON.id + "\n Title: "+res.responseJSON.title);
+            h = $("<h3 class='border-bottom border-danger'>").text("ID: " + res.responseJSON.id + 
+            "\n Title: "+res.responseJSON.title);
             p = $("<p>").text("Body: " + res.responseJSON.body);
 
             $(div).append(h,p);
@@ -161,10 +163,12 @@ $(document).on("click", ".makeList", function(){
     }).then(function(res){
         for(var i=0; i < res.length; i++){
             // console.log(res[i]);
-            var infolink =('<button class="link btn btn-info" data-link="http://jsonplaceholder.typicode.com/posts/' + res[i].id + '/comments">See Post history</button>');
-            var div = $("<div>")
-            var h1 = $("<h1>").text(res[i].title);
-            var p = $("<p>").text(res[i].body);
+            var infolink =(
+                '<button class="link btn btn-info" data-link="http://jsonplaceholder.typicode.com/posts?postId=' 
+                + res[i].id + '/comments/">See Post history</button>');
+            div = $("<div>")
+            h1 = $("<h1>").text(res[i].title);
+            p = $("<p>").text(res[i].body);
 
             $(div).append(h1,p,infolink);
 
@@ -176,7 +180,8 @@ $(document).on("click", ".makeList", function(){
 
 // This must be a document on click function since buttons are generated dynamically and are not present on inital load
 // Good practice to do for all click functions so you to not have usesles click functions
-($(document).on("click", ".link", function(){
+$(document).on("click", ".link", function(){
+    console.log(this)
     var postLink = $(this).data("link");
     console.log(postLink);
     $("#apiDump").empty();
@@ -187,14 +192,13 @@ $(document).on("click", ".makeList", function(){
         console.log(res);
         for(i=0; i<res.length; i++){
             Div = $("<div>");
-            var h1 = $("<h2>").text(res[i].postId + " To: " + res[i].name);
+            var h1 = $("<h2>").text(res[i].id + " Title: " + res[i].title);
             p = $("<p>").text(res[i].body);
-            var email = $("<h3>").text(res[i].email)
-            $(Div).append(h1, email, p);
+            $(Div).append(h1, p);
             $("#apiDump").append(Div);
         }
-        backButton =('<button class="makeList btn btn-primary" data-link="http://jsonplaceholder.typicode.com/posts">See Post history</button>');
+        backButton =('<button class="makeList btn btn-primary" data-link="http://jsonplaceholder.typicode.com/posts">See all posts</button>');
         $("#apiDump").prepend(backButton);
 
     })
-}))
+});
--- fetch.js
+++ fetch.js
@@ -9,10 +9,100 @@
 //   }).catch(function() {
 //   console.log("Booo");
 // });
-// $("#signOut_Btn").hide();
 
-// Find user and log in
+$("#signOut_Btn").hide();
+
+// When the user inputs a name it does something
 $(document).on("submit", "form", function(event){
-    console.log(event.target);
-    // $("#signOut_Btn").show();
+
+    event.preventDefault();
+    $("#signOut_Btn").show();
+
+    var name = event.target.username.value;
+
+    if(name === "Bret") {name = 1}
+    if(name === "Antonette"){name = 2}
+    if(name === "Samantha"){name = 3}
+    if(name === "Karianne"){name = 4}
+    if(name === "Kamren"){name = 5}
+    if(name === "Leopoldo_Corkery"){name = 6}
+    if(name === "Elwyn.Skiles"){name = 7}
+    if(name === "Maxime_Nienow"){name = 8}
+    if(name === "Delphine"){name = 9}
+    if(name === "Moriah.Stanton"){name = 10}
+    console.log(name);
+
+    $.ajax({
+        url: "http://jsonplaceholder.typicode.com/users/" + name,
+        method: "GET"
+
+    }).then((res) => {
+        console.log(res);
+
+        var div = $("<div class='border p-3 border-dark'>")
+        var welcome = $("<h3>").text("Welcome back! " + res.name);
+        var email = $("<h5>").text("Your primary email is: " + res.email);
+        var phone = $("<h5>").text("Your primary phone number is: " + res.phone);
+        var website = $("<a href=" + res.website + ">Link to website</a>");
+        var albumsLink = $("<button class='m-2 albumLink' type='button' data-link='http://jsonplaceholder.typicode.com/users/" + res.id + "/albums/?userId=" + res.id + "'>").text("albums");
+        var postTitleLink = $("<button class='m-2 titleLink' type='button' data-link='http://jsonplaceholder.typicode.com/users/" + res.id + "/posts/?userId=" + res.id + "'>").text("titles")
+        $(div).append(welcome, email, phone, website,albumsLink, postTitleLink);
+        $("#signedIn").append(div);
+
+    }).catch((err) => {
+        console.error(err);
+        $("#signOut_Btn").hide();
+        alert("Your input did not match any usernames!!! \n Try again.");
+    })
+    
+});
+
+// Too see a users albums listed
+$(document).on("click", ".albumLink", function(){
+    var link = $(this).data("link");
+    console.log(link);
+    $.ajax({
+        url: link,
+        method: "GET"
+    }).then(function(res){
+        console.log(res);
+        $("#apiDump").empty();
+
+        for(var i=0; i < res.length; i++){
+            div = $("<div>");
+            var h = $("<h4>").text("Album title: " + res[i].title);
+            $(div).append(h);
+            $("#apiDump").append(div);
+        }
+
+    })
+
+});
+
+// See post titles
+$(document).on("click", ".titleLink", function(){
+    link = $(this).data("link");
+    console.log(link);
+    $.ajax({
+        url: link,
+        method: "GET"
+    }).then(function(res){
+        console.log(res);
+        $("#apiDump").empty();
+
+        for(var i=0; i < res.length; i++){
+            div = $("<div>");
+            var h = $("<h4>").text("Album title: " + res[i].title);
+            $(div).append(h);
+            $("#apiDump").append(div);
+        }
+
+    })
+    
+})
+
+$(document).on("click", "#signOut_Btn", function(){
+    $("#signedIn").empty();
+    $("#apiDump").empty();
+    $("#signOut_Btn").hide();
 })
